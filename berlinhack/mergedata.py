253cadb5cbab4b3d7059f648a613b5df68b5ef24
lukovnikov <lukovnikov@outlook.com>
2017-09-12 17:41:44 +0200

    * added crimes 2015

--- berlinhack/mergedata.py
+++ berlinhack/mergedata.py
@@ -5,9 +5,10 @@ import re
 
 
 def run():
+    ret = None
     frames = []
-    filter = re.compile(r'.+stop-and-search\.csv')
-    for root, dirs, files in os.walk("../data/"):
+    filter = re.compile(r'.+street\.csv')
+    for root, dirs, files in os.walk("../data/alllondon"):
         path = root.split(os.sep)
         print((len(path) - 1) * '---', os.path.basename(root))
         for file in files:
@@ -17,6 +18,16 @@ def run():
                 frame = pd.DataFrame.from_csv(filep)
                 frames.append(frame)
     print("loaded {} frames".format(len(frames)))
+    ret = pd.concat(frames)
+    print("merged files contain {} rows".format(len(ret)))
+    print(ret)
+    ret.to_csv("../data/all-street-2015.csv")
+    print("saved")
+    lastoutcomes = ret["Last outcome category"]
+    ulastoutcomes = lastoutcomes.unique()
+    print(len(ulastoutcomes))
+    print(ulastoutcomes)
+    return ret
 
 
 
